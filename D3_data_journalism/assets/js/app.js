13b6bd6dd9fb0f503f19d902ab08deadcbeeb91e
Brittany Clemmons <brittanyclemmons@gmail.com>
2020-02-18 02:29:28 -0600

    Centered scatter better on page, still in progress

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -2,7 +2,7 @@
 var svgWidth = 960;
 var svgHeight = 500;
 
-var margin = {top: 20, right: 40, bottom: 60, left: 100};
+var margin = {top: 20, right: 175, bottom: 70, left: 45};
 
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
@@ -18,21 +18,19 @@ var chartGroup = svg.append("g")
 // Importing Data
 d3.csv("./assets/data/data.csv").then((healthData) => {
 
-    console.log(healthData);
-
     // Parse Data as numbers
     healthData.forEach((data) => {
-        data.age = +data.age;
-        data.income = +data.income;
+        data.poverty = +data.poverty;
+        data.healthcare = +data.healthcare;
     });
 
     // Create scale functions
     var xLinearScale = d3.scaleLinear()
-        .domain([30, d3.max(healthData, d => d.age)])
+        .domain([8, d3.max(healthData, d => d.poverty)])
         .range([0, width]);
 
     var yLinearScale = d3.scaleLinear()
-        .domain([0, d3.max(healthData, d => d.income)])
+        .domain([0, d3.max(healthData, d => d.healthcare)])
         .range([height, 0]);
 
     // Create axis functions
@@ -52,8 +50,8 @@ d3.csv("./assets/data/data.csv").then((healthData) => {
     .data(healthData)
     .enter()
     .append("circle")
-    .attr("cx", d => xLinearScale(d.age))
-    .attr("cy", d => yLinearScale(d.income))
+    .attr("cx", d => xLinearScale(d.poverty))
+    .attr("cy", d => yLinearScale(d.healthcare))
     .attr("r", "15")
     .attr("fill", "pink")
     .attr("opacity", ".5");
@@ -61,16 +59,16 @@ d3.csv("./assets/data/data.csv").then((healthData) => {
     // Create axes labels
     chartGroup.append("text")
       .attr("transform", "rotate(-90)")
-      .attr("y", 0 - margin.left + 40)
+      .attr("y", 0 - margin.left +0)
       .attr("x", 0 - (height / 2))
       .attr("dy", "1em")
       .attr("class", "axisText")
-      .text("Income");
+      .text("Healthcare");
 
     chartGroup.append("text")
       .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
       .attr("class", "axisText")
-      .text("Age");
+      .text("Poverty");
   }).catch(function(error) {
     console.log(error);
 });
